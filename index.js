54754d05fd65135ad02aa42f786ccbdf13b80472
Rune Tvilum <rune@addin.dk>
2015-12-15 19:58:02 +0100

    synchronicer

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
     'use strict';
     var argv = require('minimist')(process.argv.slice(2)),
         jf = require('jsonfile'),
+        xmlparser = require('express-xml-bodyparser'),
         windows1252 = require('windows-1252'),
         iconv = require('iconv-lite'),
         crypto = require('crypto'),
@@ -41,8 +42,44 @@
         transport,
         checkAdmin,
         schemaPostPut,
+        nanoClean = require('nano')({
+            "url": "http://localhost:5984"
+        }),
         sinh = function sinh(x) {
             return (Math.exp(x) - Math.exp(-x)) / 2;
+        },
+        find = function (id, db) {
+            return new Promise(function (resolve, reject) {
+                db.list({
+                    include_docs: true
+                }, function (err, body) {
+                    if (err) {
+                        reject("fejl ved hentning af dokumenter");
+                    }
+                    for (var i = 0; i < body.rows.length; i++) {
+                        var row = body.rows[i];
+                        if (row.doc && row.doc.properties && row.doc.properties.Dato) {
+                            var dato = new Date(row.doc.properties.Dato);
+                            var docid = dato.getTime();
+                            if (docid === id) {
+                                resolve(row.doc);
+                            }
+                        }
+                    }
+                    reject();
+                });
+            });
+        },
+        insert = function (doc, db) {
+            return new Promise(function (resolve, reject) {
+                db.insert(doc, doc._id, function (err, body) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(body);
+                    }
+                });
+            });
         },
         transformPoint = function (p, z2, tx, ty, extent) {
             var x = (p[0] / extent + tx), //Math.round(extent * (p[0] * z2 - tx)),
@@ -166,7 +203,88 @@
             }
         }
     });
+
     db_admin = nano.db.use("admin");
+    auth = function (req, res, next) {
+        function unauthorized(res) {
+            res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+            return res.send(401);
+        }
+
+        function cookie(req, res, next) {
+            var couchdb = require('nano')({
+                cookie: req.headers.cookie,
+                url: url_5986
+            });
+            couchdb.session(function (err, session, headers) {
+                if (err) {
+                    return unauthorized(res);
+                }
+                req.userCtx = session.userCtx;
+                if (headers && headers['set-cookie']) {
+                    res.set('set-cookie', headers['set-cookie']);
+                }
+                return next();
+            });
+        }
+
+        function login(user, req, res, next) {
+            var couchdb = require('nano')({
+                url: url_5986
+            });
+            couchdb.auth(user.name, user.pass, function (err, body, headers) {
+                if (err) {
+                    return unauthorized(res);
+                }
+                req.userCtx = body;
+                if (headers && headers['set-cookie']) {
+                    res.set('set-cookie', headers['set-cookie']);
+                }
+                return next();
+            });
+        }
+        var user = basicAuth(req);
+        if (typeof user === 'undefined') {
+            if (req.headers && req.headers.cookie) {
+                return cookie(req, res, next);
+            }
+            return unauthorized(res);
+        }
+        return login(user, req, res, next);
+    };
+    app.post('/synchronicer/:db', auth, xmlparser(), function (req, res) {
+        if (!req.params.db) {
+            return res.status(500).send('Database ikke angivet');
+        }
+        if (!(req.body && req.body.import && req.body.import.tour && req.body.import.tour.length > 0)) {
+            return res.status(500).send('XML indeholder ikke tour');
+        }
+        var tour = req.body.import.tour[0];
+        if (!(tour.registration && tour.registration.length > 0)) {
+            return res.status(500).send('tour indeholder ikke registration');
+        }
+        var registration = tour.registration[0];
+        if (!(registration.hasOwnProperty('$') && registration['$'].hasOwnProperty('reg_reference') && registration['$'].hasOwnProperty('reg_status'))) {
+            return res.status(500).send('registration indeholder ikke reg_reference og reg_status');
+        }
+        var id = parseInt(registration['$'].reg_reference);
+        var reg_status = parseInt(registration['$'].reg_status);
+        if (reg_status !== 3) {
+            return res.status(500).send('reg_status er ikke lig med 3');
+        }
+        var db = nanoClean.use(req.params.db);
+        find(id, db).then(function (doc) {
+            doc.properties.Status = 'Udbedres';
+            console.log(doc);
+            return insert(doc, db);
+        }).then(function (body) {
+            console.log(body);
+            res.json(body);
+        }).catch(function (err) {
+            console.log(err);
+            res.sendStatus(500);
+        })
+    });
 
     app.use('/tilestream', function (req, res) {
 
@@ -263,53 +381,7 @@
 
     app.use(express["static"](__dirname)); //  "public" off of current is root
 
-    auth = function (req, res, next) {
-        function unauthorized(res) {
-            res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-            return res.send(401);
-        }
-
-        function cookie(req, res, next) {
-            var couchdb = require('nano')({
-                cookie: req.headers.cookie,
-                url: url_5986
-            });
-            couchdb.session(function (err, session, headers) {
-                if (err) {
-                    return unauthorized(res);
-                }
-                req.userCtx = session.userCtx;
-                if (headers && headers['set-cookie']) {
-                    res.set('set-cookie', headers['set-cookie']);
-                }
-                return next();
-            });
-        }
-
-        function login(user, req, res, next) {
-            var couchdb = require('nano')({
-                url: url_5986
-            });
-            couchdb.auth(user.name, user.pass, function (err, body, headers) {
-                if (err) {
-                    return unauthorized(res);
-                }
-                req.userCtx = body;
-                if (headers && headers['set-cookie']) {
-                    res.set('set-cookie', headers['set-cookie']);
-                }
-                return next();
-            });
-        }
-        var user = basicAuth(req);
-        if (typeof user === 'undefined') {
-            if (req.headers && req.headers.cookie) {
-                return cookie(req, res, next);
-            }
-            return unauthorized(res);
-        }
-        return login(user, req, res, next);
-    };
+    
 
     //region Login
     app.post('/api/signin', function (req, res) {
@@ -1951,58 +2023,58 @@
             if (err) {
                 return res.status(err.status_code || 500).send(err);
             }
-            var csv='"_id";"_rev"';
+            var csv = '"_id";"_rev"';
             var columns = [];
             var attachments = [];
             for (var i = 0; i < body.rows.length; i++) {
                 var row = body.rows[i];
-                if (row.id.substring(0, 1) !== '_'){
-                    if(row.doc.hasOwnProperty('properties')) {
-                        for(var key in row.doc.properties){
-                            if(columns.indexOf(key)===-1){
+                if (row.id.substring(0, 1) !== '_') {
+                    if (row.doc.hasOwnProperty('properties')) {
+                        for (var key in row.doc.properties) {
+                            if (columns.indexOf(key) === -1) {
                                 columns.push(key);
                             }
                         }
                     }
                     if (row.doc.hasOwnProperty("_attachments")) {
-                        for(var key in row.doc['_attachments']){
-                            if(attachments.indexOf(key)===-1){
+                        for (var key in row.doc['_attachments']) {
+                            if (attachments.indexOf(key) === -1) {
                                 attachments.push(key);
                             }
-                        }                        
+                        }
                     }
                 }
             }
-            for(var j=0;j<columns.length;j++){
+            for (var j = 0; j < columns.length; j++) {
                 var column = columns[j];
-                csv+=';"'+column+'"';
+                csv += ';"' + column + '"';
             }
-            for(var j=0;j<attachments.length;j++){
+            for (var j = 0; j < attachments.length; j++) {
                 var column = attachments[j];
-                csv+=';"'+column+'"';
+                csv += ';"' + column + '"';
             }
-            csv+='\n';
+            csv += '\n';
             for (var i = 0; i < body.rows.length; i++) {
                 var row = body.rows[i];
                 if (row.id.substring(0, 1) !== '_') {
-                    csv += '"'+row.doc["_id"]+'";"'+row.doc["_rev"]+'"';
-                    for(var j=0;j<columns.length;j++){
+                    csv += '"' + row.doc["_id"] + '";"' + row.doc["_rev"] + '"';
+                    for (var j = 0; j < columns.length; j++) {
                         var column = columns[j];
                         var v = "";
                         if (row.doc.hasOwnProperty("properties") && row.doc.properties.hasOwnProperty(column)) {
                             v = row.doc.properties[column];
                         }
-                        csv+=';"'+v+'"';
+                        csv += ';"' + v + '"';
                     }
-                    for(var j=0;j<attachments.length;j++){
+                    for (var j = 0; j < attachments.length; j++) {
                         var column = attachments[j];
                         var v = "";
                         if (row.doc.hasOwnProperty("_attachments") && row.doc["_attachments"].hasOwnProperty(column)) {
                             v = "https://geo.os2geo.dk/couchdb/" + req.params.database + "/" + row.id + "/" + column;
                         }
-                        csv+=';"'+v+'"';
+                        csv += ';"' + v + '"';
                     }
-                    csv+='\n';
+                    csv += '\n';
                 }
             }
             res.header('Content-Type', 'text/csv');
