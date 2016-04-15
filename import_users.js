6a8ef15bf6b7bfc9d89e71637abf970bb3cbc675
Rune Tvilum <rune@addin.dk>
2016-04-15 12:12:40 +0200

    a

--- import_users.js
+++ import_users.js
@@ -1,6 +1,6 @@
 /*global require, console,process*/
 var config = require('./config.json');
-var nano = require('nano')({
+var bigcouch = require('nano')({
     url: 'http://localhost:5986',
     requestDefaults: {
         auth: {
@@ -9,11 +9,42 @@ var nano = require('nano')({
         }
     }
 });
-var db = nano.use('_users');
-db.list({
+var couchdb = require('nano')({
+    url: 'http://couchdb:5984',
+    requestDefaults: {
+        auth: {
+            user: config.couchdb.user,
+            pass: config.couchdb.password
+        }
+    }
+});
+var bigcouch_users = bigcouch.use('_users');
+var couchdb_users = couchdb.use('_users');
+var users = [];
+var insert = function () {
+    if (users.length > 0) {
+        var row = users.pop();
+        var doc = row.doc;
+        couchdb_users.get(doc._id, function (err, body) {
+            if (err) {
+                delete doc._rev;
+                couchdb_users.insert(doc, doc._id, function (err, body) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(body);
+                    }
+                    insert();
+                });
+            } else {
+                insert();
+            }
+        });
+    }
+};
+bigcouch_users.list({
     include_docs: true
 }, function (err, body) {
-    body.rows.forEach(function(doc){
-        console.log(doc);
-    });    
+    users = body.rows;
+    insert();
 });
