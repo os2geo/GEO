ab0c46e8be876194ac541fe7b82fb7a58ebe79a2
Rune Tvilum <rune@addin.dk>
2016-04-15 14:41:21 +0200

    a

---
+++ import_replicate.js
@@ -0,0 +1,34 @@
+/*global require, console,process*/
+var config = require('./config.json');
+var bigcouch = require('nano')({
+    url: 'http://localhost:5986',
+    requestDefaults: {
+        auth: {
+            user: config.couchdb.user,
+            pass: config.couchdb.password
+        }
+    }
+});
+
+var all_dbs = [];
+var replicate = function () {
+    if (all_dbs.length > 0) {        
+        var db = all_dbs.pop();
+        console.log(db);
+        nano.db.replicate(db, 'http://' + config.couchdb.user + ':' + config.couchdb.password + '@couchdb:5984/' + db, { 
+            create_target: true 
+        }, function (err, body) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(body);
+                }
+                replicate();
+            });
+
+    }
+};
+bigcouch.db.get('_all_dbs', function (err, body) {
+    all_dbs = body;
+    replicate();
+});
