63bf7e5e65a9f03c3d93e023637221e4938fa044
Rune Tvilum <rune@addin.dk>
2016-04-15 11:58:28 +0200

    import_users

---
+++ import_users.js
@@ -0,0 +1,19 @@
+/*global require, console,process*/
+var config = require('config.json');
+var nano = require('nano')({
+    url: 'http://localhost:5986',
+    requestDefaults: {
+        auth: {
+            user: config.couchdb.user,
+            pass: config.couchdb.password
+        }
+    }
+});
+var db = nano.use('_users');
+db.list({
+    include_docs: true
+}, function (err, body) {
+    body.rows.forEach(function(doc){
+        console.log(doc);
+    });    
+});
