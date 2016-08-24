ca8aa0cd0e45e265d15b4dc9291008137f6eb0ee
Rune Tvilum <rune@addin.dk>
2016-08-24 11:06:57 +0200

    count docs

--- src/www/organizations/list/list-controllers.js
+++ src/www/organizations/list/list-controllers.js
@@ -1,11 +1,23 @@
 (function (window, angular, console) {
     'use strict';
-    angular.module('myApp.controllers').controller('organizations-list', ['$scope', '$rootScope', '$http', 
-        function ($scope, $rootScope, $http) {
+    angular.module('myApp.controllers').controller('organizations-list', ['$scope', '$rootScope', '$http','$q', 
+        function ($scope, $rootScope, $http, $q) {
             var getDatabases = function (organization) {
                 $http.get('/api/organization/' + organization.id+'/databases').
                 success(function (data, status, headers, config) {
                     organization.databases = data.rows.length;
+                    organization.size=0;
+                    organization.docs=0;
+                    var promises = [];
+                    for(var i=0;i<data.rows.length;i++){
+                        promises.push($http.get('/couchdb/db-'+data.rows[i].id));
+                    }
+                    $q.all(promises).then(function(data){
+                        for(var i=0;i<data.length;i++){
+                            organization.docs += data[i].data.doc_count;
+                            organization.size += data[i].data.disk_size;
+                        }
+                    });
                 }).
                 error(function (data, status, headers, config) {
                     console.log(data);
@@ -21,6 +33,7 @@
                     console.log(data);
                 });
             };
+            
             for (var i = 0; i < $rootScope.profile.user.organizations.length; i++) {
                 var organization = $rootScope.profile.user.organizations[i];
                 getDatabases(organization);
